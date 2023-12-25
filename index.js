525425f123cc10c5ab0d735df9808fca4ed210df
TahidulAlam <tahid.cse@gmail.com>
2023-12-25 23:19:01 +0600

    update

--- index.js
+++ index.js
@@ -2,39 +2,26 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 // tahidcse
 // 9IWQ1OWv9QnAVvqk
-const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
-  credentials: true,
-  optionSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: [
+//     "http://localhost:5173",
+//     "http://localhost:5174",
+//     "https://taskyear.web.app",
+//   ],
+//   credentials: true,
+//   optionSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token;
-
-  if (!token) {
-    return res.status(401).send({ message: "unauthorized access" });
-  }
-
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
-    if (err) {
-      console.log(err);
-      return res.status(401).send({ message: "unauthorized access" });
-    }
-
-    req.user = decoded;
-    next();
-  });
-};
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w9927an.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -61,17 +48,17 @@ async function run() {
         console.log(error);
       }
     });
+
     app.post("/api/addTask", async (req, res) => {
       try {
         const data = req.body;
-        console.log(data);
         const result = await taskCollection.insertOne(data);
         res.send(result);
       } catch (error) {
         console.log(error);
       }
     });
-    app.delete("/api/addTask/:id", verifyToken, async (req, res) => {
+    app.delete("/api/addTask/:id", async (req, res) => {
       try {
         const id = req.params.id;
         const query = { _id: new ObjectId(id) };
@@ -81,7 +68,7 @@ async function run() {
         console.log(error);
       }
     });
-    app.patch("/api/addtask/:id", verifyToken, async (req, res) => {
+    app.patch("/api/addTask/:id", async (req, res) => {
       try {
         const defaultData = req.body;
         const id = req.params.id;
@@ -100,7 +87,26 @@ async function run() {
         console.log(error);
       }
     });
+    app.put("/api/addTask/:taskId", async (req, res) => {
+      try {
+        const taskId = req.params.taskId;
+        const updatedData = req.body;
 
+        const filter = { _id: new ObjectId(taskId) };
+        const update = { $set: updatedData };
+
+        const result = await taskCollection.updateOne(filter, update);
+
+        if (result.matchedCount === 0) {
+          return res.status(404).json({ message: "Task not found" });
+        }
+
+        res.json({ message: "Task updated successfully" });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error updating task" });
+      }
+    });
     console.log(
       "Taskyear Pinged your deployment. You successfully connected to MongoDB!"
     );
